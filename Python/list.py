a61195352b9deebd15de0272dbc219f8362b7fb5
Ritanshu1104 <ritanshupm@gmail.com>
2026-06-03 21:46:06 +0530

    Create list.py

---
+++ Python/list.py
@@ -0,0 +1,21 @@
+list=["Apple", 1, 10.0, False, "Ritanshu"]
+print(list)
+print(list[0])
+list[0]="Grapes"    #changing the elemnents are allowed in list but not in strings
+print(list[0])
+
+
+
+l1=[1,8,7,2,21,15]
+l1.sort()
+l1.reverse()
+l1.append(8)
+l1.insert(3,24)
+l1.pop(1)
+l1.remove(1)
+print(l1)
+
+
+b=[1,2,3,4]
+b.sort(reverse=True)
+print(b)
