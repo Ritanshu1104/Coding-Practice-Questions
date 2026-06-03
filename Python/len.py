d82afa5ab98c7727e9b95cc2d845017c67a08c22
Ritanshu1104 <ritanshupm@gmail.com>
2026-06-03 21:45:19 +0530

    Create len.py

---
+++ Python/len.py
@@ -0,0 +1,8 @@
+name="ritanshu mahajan"
+print(len(name))
+print(name.title())
+print(name.capitalize())
+print(name.upper())
+print(name.startswith("r"))
+print(name.endswith("m"))
+print(name.replace("ritanshu","anushka"))
