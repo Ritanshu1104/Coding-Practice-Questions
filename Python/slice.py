d85f78297afdcdd54e4f1670242523c718b44530
Ritanshu1104 <ritanshupm@gmail.com>
2026-06-04 10:53:36 +0530

    Slicing

---
+++ Python/slice.py
@@ -0,0 +1,27 @@
+name = 'Ritanshu Mahajan '
+
+nameslice= name[0:3]
+print(nameslice)
+
+negslice=name[-3:-1]
+print(negslice)
+
+print(name[:])
+
+print(name[0:10000])
+
+print(name+name)
+
+print(name*2)
+
+print(name.center(150))
+print(name.rjust(150))
+
+print(name.title())
+
+print(name.upper())
+
+print(name.lstrip())    #left side white space is removed
+print(name.rstrip())    #right side white space is removed
+
+print(name.find())
