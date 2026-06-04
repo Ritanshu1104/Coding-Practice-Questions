ce46e48ff12c99d2f4dc2b389e368eb0dbbf3a11
Ritanshu1104 <ritanshupm@gmail.com>
2026-06-04 10:47:37 +0530

    Loops

---
+++ Python/loops.py
@@ -0,0 +1,32 @@
+#While Loop
+i=1
+while(i<=5):
+    print(i)
+    i+=1
+
+print("")
+
+l = ["Ritanshu",1,False,4,"Yash"]
+i=0
+while(i<len(l)):
+    print(l[i])
+    i+=1
+print("")
+
+#For Loop
+li = ["Ritanshu",1,False,4,"Yash",True]
+for i in li:
+    print(i)
+
+print("")
+
+tup=(1,2,3,4)
+for i in tup:
+    print(i)
+
+print("")
+
+str = "Ritanshu"
+for  i in str:
+    print(i)
+   
